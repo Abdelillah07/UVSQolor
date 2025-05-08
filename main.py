c3e0921fbb0d75e2e7b6521e90c73c6acb1a2ab4
Abdelillah07 <abdelbess110@gmail.com>
2025-05-08 19:00:09 +0200

    Abdelillah

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from fonctions import charger, callback_vert, callback_gris, callback_luminosite, callback_constraste, callback_flou, callback_flou_de_gauss, callback_detection_bord, callback_fusion
+from fonctions import charger, undo, not_undo, fen_sauver, callback_vert, callback_gris, callback_luminosite, callback_constraste, callback_flou, callback_flou_de_gauss, callback_detection_bord, callback_fusion
 
 # Création de la fenêtre principale
 fenetre_principale = tk.Tk()
@@ -13,6 +13,14 @@ fenetre_principale.config(menu=menubar)
 # crée un menu
 file_menu1 = tk.Menu(menubar, tearoff=False)
 file_menu2 = tk.Menu(menubar, tearoff=False)
+file_menu_filtre_couleur = tk.Menu(menubar, tearoff=False)
+
+# Bouton revenir en arrière
+button_undo = tk.Button(fenetre_principale, text="🔙", command=lambda: undo(fenetre_principale))
+button_undo.pack()
+
+button_not_undo = tk.Button(fenetre_principale, text="🔜", command= lambda: not_undo(fenetre_principale))
+button_not_undo.pack()
 
 # Rajouter l'onglet Fichier
 menubar.add_cascade(
@@ -26,17 +34,28 @@ file_menu1.add_command(
     command=lambda : charger(fenetre_principale)
 )
 
+file_menu1.add_command(
+    label='Enregitrer',
+    command= fen_sauver
+)
+
 # Rajouter l'onglet Effets
 menubar.add_cascade(
     label="Effets",
     menu=file_menu2
 )
 
-file_menu2.add_command(
+file_menu2.add_cascade(
+    label="Filtres de Couleurs",
+    menu=file_menu_filtre_couleur
+)
+
+file_menu_filtre_couleur.add_command(
     label='Filtre Vert',
     command=lambda : callback_vert(fenetre_principale)
 )
-file_menu2.add_command(
+
+file_menu_filtre_couleur.add_command(
     label='Filtre Gris',
     command=lambda : callback_gris(fenetre_principale)
 )
